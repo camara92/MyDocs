b3ae19255dc7899ae7df5717c816d022b0b1d32c
camara92 <88945535+camara92@users.noreply.github.com>
2022-09-28 12:37:52 +0200

    home page styling

--- assets/app.js
+++ assets/app.js
@@ -11,32 +11,7 @@ import './styles/app.scss';
 // start the Stimulus application
 import './bootstrap';
 
-// slider : 
-var slides = document.getElementsByClassName('slide');
-var slideCount = slides.length;
-var i = 0;
-
-function moveLeft() {
-  i++; // i = i + i 
-  if (i < slideCount) {
-    slides[i].style.left = "0";
-    slides[i - 1].style.left = "-100%";
-    console.log("advanced i = " + i);
-    document.getElementById("next").slideDown(100);
-  } else { 
-  i = 0; // Set current slide back to zero
-    slides[i].style.left = "0";
-    slides[slideCount - 1].style.left = "-100%";
-
-    console.log("back to start");
-  
-    for (var x = 1; x < slideCount -1; x++) { 
-     slides[x].style.left = "100%";
-    }
-    
-  }
-}
-// Run the function, move left, when clicked on next// 
-document.getElementById("next").onclick = moveLeft;
+// card comments home page 
+		
 
 
